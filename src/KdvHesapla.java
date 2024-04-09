7cd31dbad194cbaa2e228d13e39988b2a2c5231d
Aferin <aferfidan@gmail.com>
2024-04-10 00:39:49 +0300

    kdv hesaplama eklendi

---
+++ src/KdvHesapla.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class KdvHesapla {
+    public static void main(String[] args) {
+ 
+        Scanner tara = new Scanner(System.in);
+
+        System.out.print("Lütfen KDV'siz Tutarı Girin: ");
+
+        double anapara, kdv, total;
+
+        anapara = tara.nextDouble();
+
+        kdv = anapara*18/100;
+
+        total = kdv + anapara;
+
+        System.out.println("KDV\'siz Fiyat = "+ anapara +"\nKDV\'li Fiyat = "+ total +"\nKDV Tutarı = "+ kdv);
+
+
+    }
+}
