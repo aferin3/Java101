0ec8e32d69706292ec2ba5734d4f978d857fa3f1
Aferin <aferfidan@gmail.com>
2024-04-10 00:25:24 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      int matematik,fizik,kimya,turkce,tarih,muzik;
+
+      System.out.print("Matematik: ");
+      Scanner tara = new Scanner(System.in);
+      matematik = tara.nextInt();
+      System.out.print("Fizik:  ");
+      fizik = tara.nextInt();
+      System.out.print("Kimya: ");
+      kimya = tara.nextInt();
+      System.out.print("Turkce: ");
+      turkce = tara.nextInt();
+      System.out.print("Tarih: ");
+      tarih = tara.nextInt();
+      System.out.print("Muzik: ");
+      muzik = tara.nextInt();
+      float ortalama = (matematik+fizik+kimya+turkce+tarih+muzik)/6f;
+      boolean durum = ortalama>=60;
+      String gecme = durum ? "Geçtiniz": "Kaldınız";
+
+      System.out.println("notunuz "+ortalama+" "+ gecme);
+
+
+
+    }
+}
